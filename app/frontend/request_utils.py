f8b00a6b71844407c3cd87315eac93933c8ad364
Andrey Timonin <super.timonin2013@gmail.com>
2024-12-30 00:04:32 +0300

    Add: implementation of /fit /models in streamlit

--- app/frontend/request_utils.py
+++ app/frontend/request_utils.py
@@ -14,3 +14,11 @@ async def post_data(endpoint:str, input_data, timeout=0):
 			# if url == "http://127.0.0.1:8000/api/v1/models/predict":
 			#     print(f"Predict is done in {time.time():.2f}")
 			return await response.json()
+
+async def get_data(endpoint:str):
+    # print(f'Начал выполнение: {idx}')
+    url = f'{API_URL}/{endpoint}'
+    print(url)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
--- app/frontend/streamlit_app.py
+++ app/frontend/streamlit_app.py
@@ -4,53 +4,116 @@ from zipfile import ZipFile
 
 import plotly.express as px
 import asyncio
-from request_utils import post_data
+from request_utils import post_data, get_data
+
+import time
 
 st.title("Team 61. Deepfake-Classification.")
 st.header("Загрузка датасета")
 st.text('Загрузите ZIP-архив с вашими данными для обучения модели. \nСтруктура каталога и имена директорий внутри архива должны соответствовать формату ниже:')
 st.text("""
-		.zip
-		├──dataset
-		├──┴─test
-		├────┼─FAKE
-		├────┴─REAL
-		├──┴─train
-		├────┼─FAKE
-		└────┴─REAL""")
+        .zip
+        ├──dataset
+        ├──┴─test
+        ├────┼─FAKE
+        ├────┴─REAL
+        ├──┴─train
+        ├────┼─FAKE
+        └────┴─REAL""")
 
 uploaded_file = st.file_uploader("Выберите ZIP-архив", type=['zip'])
 
 
 if uploaded_file is not None:
-	# Сохраняем zip-архив от пользователя в папку
-	with open(r"..\data\load_user_dataset.zip", "wb") as f:
-		f.write(uploaded_file.getbuffer())
+    # Сохраняем zip-архив от пользователя в папку
+    with open(r"..\data\load_user_dataset.zip", "wb") as f:
+        f.write(uploaded_file.getbuffer())
 
-	with ZipFile(r"..\data\load_user_dataset.zip", 'r') as zObject: 
+    with ZipFile(r"..\data\load_user_dataset.zip", 'r') as zObject: 
   
     # Extracting all the members of the zip  
     # into a specific location. 
-		zObject.extractall(path=r"..\data")
-		st.write("ZIP-file saved and unziped")
+        zObject.extractall(path=r"..\data")
+        st.write("ZIP-file saved and unziped")
 
 if uploaded_file is not None:
-	st.header(f"Анализ данных из {uploaded_file.name}")
-	response_eda = asyncio.run(post_data('eda', input_data={}))
+    st.header(f"Профиль данных из {uploaded_file.name}")
+    response_eda = asyncio.run(post_data('eda', input_data={}))
 
-	df = pd.DataFrame(response_eda)
-	df = df.T.copy()
-	df['mean_red'] = df['mean_rgb'].apply(lambda x: x[0])
-	df['mean_green'] = df['mean_rgb'].apply(lambda x: x[1])
-	df['mean_blue'] = df['mean_rgb'].apply(lambda x: x[2])
+    df = pd.DataFrame(response_eda)
+    df = df.T.copy()
+    df['mean_red'] = df['mean_rgb'].apply(lambda x: x[0])
+    df['mean_green'] = df['mean_rgb'].apply(lambda x: x[1])
+    df['mean_blue'] = df['mean_rgb'].apply(lambda x: x[2])
 
-	df['std_red'] = df['std_rgb'].apply(lambda x: x[0])
-	df['std_green'] = df['std_rgb'].apply(lambda x: x[1])
-	df['std_blue'] = df['std_rgb'].apply(lambda x: x[2])
-	df = df[['fake_cnt', 'real_cnt', 
-		     'avg_size', 'min_size', 'max_size', 
-			 'mean_red', 'mean_green', 'mean_blue', 
-			 'std_red', 'std_green', 'std_blue']].T
-	st.table(df)
+    df['std_red'] = df['std_rgb'].apply(lambda x: x[0])
+    df['std_green'] = df['std_rgb'].apply(lambda x: x[1])
+    df['std_blue'] = df['std_rgb'].apply(lambda x: x[2])
+    df = df[['fake_cnt', 'real_cnt', 
+             'avg_size', 'min_size', 'max_size', 
+             'mean_red', 'mean_green', 'mean_blue', 
+             'std_red', 'std_green', 'std_blue']].T
+    st.table(df)
+    #st.write(response_eda)
 
-	st.write(response_eda)
+st.header(f"Настройка нейронной сети")
+
+id = st.text_input("Введите идентификатор (ID) для нейронной сети")
+id = id.lower().replace(" ", "")
+
+st.write(id)
+
+type_nn_pretrain = st.selectbox("Выберите архитектуру предобученной НС",
+                                ("ResNet18", "MobileNetV3"))
+
+end_activation_function = "Sigmoid"
+
+batch_size = st.select_slider("Выберите размер батча (batch_size)", 
+                               options=[2 ** j for j in range(5, 11)])
+    
+lr = st.slider("Выберите размер шага обучения (learning rate)",
+                min_value=0.01,
+                max_value=0.1,
+                step=0.01)
+weight_decay = st.slider("Выберите величину коэффициента регуляризации ($\lambda$)",
+              min_value=0.,
+              max_value=1.,
+              step=0.01)
+
+
+left, middle, right = st.columns(3)
+
+if left.button("Обучить нейронную сеть", use_container_width=True):
+    if id != '' and id is not None:
+        since = time.time()
+        response_fit = asyncio.run(post_data('fit', input_data={
+            'id': id,
+            'hyperparameters' : {
+                'type_nn_pretrain': type_nn_pretrain,
+                'end_activation_function': end_activation_function,
+                'batch_size': batch_size,
+                'lr': lr,
+                'C': weight_decay,
+            }
+        }))
+        time_elapsed = time.time() - since
+        if response_fit['message'] == f"Model with id '{id}' already exist. Enter another id.":
+            st.write('Модель с таким ID уже существует. Введите другой ID.')
+        else:
+            left.write(f"Нейронная сеть обучена. Время обучения: {time_elapsed:.2f} секунд.")
+    else:
+        left.markdown("Введите ID.")
+
+with st.sidebar:
+    if st.button("Вывести информацию об обученных моделях", use_container_width=False):
+        response_models = asyncio.run(get_data('models'))
+        if response_models[0]:
+            st.write(response_models[0])
+        else:
+            st.write('Нет обученных моделей')
+
+
+
+
+
+
