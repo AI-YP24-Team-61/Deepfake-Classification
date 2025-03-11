0022fb9017bbff74105c0e8838a85ced8b7f04d6
Timonin Andrey <an-timwork@yandex.ru>
2025-03-11 22:06:53 +0300

    Add: .ipynb with upgrading baseline from accuracy 0.75 to 0.98 using resnet18 finetuning

---
+++ utils/handling_data.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+
+def summarize_diagnostics(history):
+    """
+    Функция для отрисовки значений LOSS и ACCURACY на каждой эпохе
+    """
+    fig,ax=plt.subplots(2)
+    fig.suptitle("Loss")
+    fig.tight_layout()
+    # plot loss
+    ax[0].set_title('Log Loss')
+    ax[0].plot(history['train_loss'], color='blue', label='Training loss')
+    ax[0].plot(history['test_loss'], color='orange', label='validation loss')
+    ax[0].legend(loc='best', shadow=True)
+    # plot accuracy
+    ax[1].set_title('Classification Accuracy')
+    ax[1].plot(history['train_acc'], color='blue', label='train')
+    ax[1].plot(history['test_acc'], color='orange', label='test')
+    ax[1].legend(loc='best', shadow=True)
+    
+def imshow(inp, title=None):
+    """Отображаем батч картинок. Лучше всего печатать по 8 картинок. Так нагляднее."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    
